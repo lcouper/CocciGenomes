b2e3a8c4b1bb424b5b6531b5c8f27ee87f35f6c7
Lisa Couper <lcouper@users.noreply.github.com>
2023-09-21 11:30:29 -0700

    Delete Cocci directory

--- Cocci/CocciRShiny_RunAndDeploy.R
+++
@@ -1,9 +0,0 @@
-### Cocci R shiny deployment ####
-
-library(shiny)
-setwd("~/Documents/Current Projects/R Shiny")
-runApp("Cocci", display.mode = "showcase")
-
-# to deploy after editing app
-library(rsconnect)
-rsconnect::deployApp('Cocci')
